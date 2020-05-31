7eaaf0fc6cb79158ca85e34f0b58449fae455345
Steven Abaco <steve@wizardrywd.com>
2020-05-31 14:11:02 -0500

    Add basic JS logic to color selection

--- colorGame.js
+++ colorGame.js
@@ -8,6 +8,27 @@ let colors = [
 ]
 
 let squares = document.querySelectorAll(".square");
+let pickedColor = colors[3];
+let colorDisplay = document.getElementById("colorDisplay");
+let messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+
 for (let i = 0; i < squares.length; i++){
+  //add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
+
+  //add click listeners to squares
+  squares[i].addEventListener("click", function(){
+  //grab color of clicked square
+  let clickedColor = this.style.backgroundColor;
+  //compare color of pickedColor
+    if(clickedColor === pickedColor){
+    messageDisplay.textContent = "Correct!";
+    } else {
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
+    }
+  });
 }
+
